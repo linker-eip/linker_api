1e15a6e2c9e190c84101d45e31ba34b0e1452013
Jérémy Calvo <jeremy.calvo@epitech.eu>
2023-04-23 12:02:10 +0200

    feat: Database initialisation, Migrations, StudentUser table

---
+++ src/entity/StudentUser.entity.ts
@@ -0,0 +1,33 @@
+import { type } from "os";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+
+export class StudentUser {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({unique: true, nullable: false})
+  email: string;
+
+  @Column({nullable: false})
+  password: string;
+
+  @Column({type: "varchar", length: 255, nullable: false})
+  firstName: string;
+
+  @Column({type: "varchar", length: 255, nullable: false})
+  lastName: string;
+
+  @Column({default: true})
+  isActive: boolean;
+
+  @Column({type: "varchar", length: 255, nullable: true})
+  picture: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+  lastConnectedAt: Date
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+  createdAt: Date
+}
