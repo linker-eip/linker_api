5072764a397d07dfaf37334eefe1126e3343b1f2
Jérémy Calvo <jeremy.calvo@epitech.eu>
2023-05-12 15:50:09 +0200

    style: removed comments

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,26 +20,22 @@ export class AuthService {
   async registerStudent(registerStudentDto: RegisterStudentDto) {
     const { email, password, firstName, lastName } = registerStudentDto;
 
-    // Vérifiez si l'utilisateur avec cet e-mail existe déjà
     const existingUser = await this.studentService.findOne( email );
 
     if (existingUser) {
       return {error : "User with email " + email + " already exists"};
     }
-    // Créez un nouvel utilisateur
+
     const newUser = new StudentUser;
     newUser.email = email;
     newUser.password = await this.hashPassword(password);
     newUser.firstName = firstName;
     newUser.lastName = lastName;
 
-    // Enregistrez le nouvel utilisateur dans la base de données
     const savedUser = await this.studentService.save(newUser);
 
-    // Générez un jeton JWT pour l'utilisateur nouvellement inscrit
     const token = jwt.sign({ email: savedUser.email }, process.env.JWT_SECRET);
 
-    // Retournez le jeton JWT pour que l'utilisateur puisse se connecter immédiatement après l'inscription
     return { token };
   }
 
