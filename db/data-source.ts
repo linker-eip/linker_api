e8a279305f982e74a1c3c8eaf4403a9eda90653d
Rayane ELOUDJEDI <rayane.eloudjedi@epitech.eu>
2023-07-12 19:19:53 +0200

    fix: postgres port

--- db/data-source.ts
+++ db/data-source.ts
@@ -3,6 +3,7 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT as unknown as number,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
